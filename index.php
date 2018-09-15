5f7bca371a0e4308b71e381b06fdde29997ab366
alexanderLazz <alexander.lazzari7@gmail.com>
2018-09-15 18:11:48 +0400

    work with cycles

--- index.php
+++ index.php
@@ -97,12 +97,11 @@ $adverts = [
         <h2 class="promo__title">Нужен стафф для катки?</h2>
         <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
         <ul class="promo__list">
-            <?php $index = 0;
-            while ($index < count($categories)) { ?>
+            <?php foreach ($categories as $key => $value) { ?>
             <li class="promo__item promo__item--boards">
-                <a class="promo__link" href="pages/all-lots.html"><?=$categories[$index] ?></a>
+                <a class="promo__link" href="pages/all-lots.html"><?=$value ?></a>
             </li>
-            <?php $index++; 
+            <?php 
             } ?>
         </ul>
     </section>
@@ -114,15 +113,15 @@ $adverts = [
             <?php foreach ($adverts as $key => $value) { ?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="<?=$value['url']; ?>" width="350" height="260" alt="<?=$value['item']; ?>">
+                    <img src="<?=$value['url'] ?>" width="350" height="260" alt="<?=$value['item'] ?>">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category"><?=$value['category']; ?></span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$value['item']; ?></a></h3>
+                    <span class="lot__category"><?=$value['category'] ?></span>
+                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$value['item'] ?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost"><?=$value['price']; ?><b class="rub">р</b></span>
+                            <span class="lot__cost"><?=$value['price'] ?><b class="rub">р</b></span>
                         </div>
                         <div class="lot__timer timer">
 
@@ -130,7 +129,8 @@ $adverts = [
                     </div>
                 </div>
             </li>
-            <?php } ?>
+            <?php 
+            } ?>
         </ul>
     </section>
 </main>
@@ -139,12 +139,11 @@ $adverts = [
 <footer class="main-footer">
     <nav class="nav">
         <ul class="nav__list container">
-            <?php $index = 0;
-            while ($index < count($categories)) { ?>
+            <?php foreach ($categories as $key => $value) { ?>
             <li class="nav__item">
-                <a href="pages/all-lots.html"><?=$categories[$index] ?></a>
+                <a href="pages/all-lots.html"><?=$value ?></a>
             </li>
-            <?php $index++; 
+            <?php
             } ?>
         </ul>
     </nav>
