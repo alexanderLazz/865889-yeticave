6c18e013001fb352ad0d86d5106a0c9aa59923fe
alexanderLazz <alexander.lazzari7@gmail.com>
2018-09-20 19:00:35 +0400

    add filters to array adverts values

--- templates/index.php
+++ templates/index.php
@@ -18,15 +18,15 @@
         <?php foreach ($adverts as $key => $value) { ?>
         <li class="lots__item lot">
             <div class="lot__image">
-                <img src="<?=$value['url'] ?>" width="350" height="260" alt="<?=$value['item'] ?>">
+                <img src="<?=htmlspecialchars($value['url']) ?>" width="350" height="260" alt="<?=htmlspecialchars($value['item']) ?>">
             </div>
             <div class="lot__info">
-                <span class="lot__category"><?=$value['category'] ?></span>
+                <span class="lot__category"><?=htmlspecialchars($value['category']) ?></span>
                 <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=htmlspecialchars($value['item']) ?></a></h3>
                 <div class="lot__state">
                     <div class="lot__rate">
                         <span class="lot__amount">Стартовая цена</span>
-                        <span class="lot__cost"><?=formatPrice($value['price']) ?></span>
+                        <span class="lot__cost"><?=formatPrice(htmlspecialchars($value['price'])) ?></span>
                     </div>
                     <div class="lot__timer timer">
 
